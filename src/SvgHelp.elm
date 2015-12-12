898c341346fcd11602cc66c7b75f779ff7c1a703
Sandi Dusic <dsandi333@gmail.com>
2015-12-12 16:04:42 +0100

    Add SvgHelp with type-checked common Svg shape functions

---
+++ src/SvgHelp.elm
@@ -0,0 +1,30 @@
+module SvgHelp where
+
+import Svg
+import Svg.Attributes as Att
+
+
+circle: (Int, Int) -> Int -> Int -> String -> String -> Svg.Svg
+circle (x, y) radius thickness stroke fill =
+    Svg.circle
+        [ toString x |> Att.cx
+        , toString y |> Att.cy
+        , toString radius |> Att.r
+        , Att.fill fill
+        , Att.stroke stroke
+        , toString thickness |> Att.strokeWidth
+        ]
+        []
+
+line: (Int, Int) -> (Int, Int) -> Int -> String -> Svg.Svg
+line (x, y) (x', y') thickness stroke =
+    Svg.line
+        [ toString x |> Att.x1
+        , toString y |> Att.y1
+        , toString x' |> Att.x2
+        , toString y' |> Att.y2
+        , Att.stroke stroke
+        , Att.fill "transparent"
+        , toString thickness |> Att.strokeWidth
+        ]
+        []
