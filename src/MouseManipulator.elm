c64e855b45e2a24a1afabbab9357f3dff14cd1f0
Sandi Dusic <dsandi333@gmail.com>
2016-04-04 18:14:58 +0200

    Add camera logic and mock camera behaviour

--- src/MouseManipulator.elm
+++ src/MouseManipulator.elm
@@ -13,6 +13,8 @@ import Svg.Attributes as Att
 import NodeBase
 import Effects exposing (Effects)
 import EffectsUtil
+import Css exposing (px)
+import CssStuff.Util as CssUtil exposing (ipx)
 
 
 -- MODEL
@@ -20,18 +22,26 @@ import EffectsUtil
 type alias Model =
     { graphMap: GraphMap.Model
     , state: State
-    , pos: (Int, Int) -- mouse position
+    , mousePos: (Int, Int)
     , size: {w: Int, h: Int}
+    , origin: {xo: Int, yo: Int} -- camera position
     }
 
 type State
     = NoOp
-    | Connecting Graph.NodeId (Int, Int)
+    | Connecting Graph.NodeId
+    | MovingCamera (Int, Int) {xo: Int, yo: Int}
+
+offsetMouse: Model -> (Int, Int)
+offsetMouse model =
+    ( fst model.mousePos - model.origin.xo
+    , snd model.mousePos - model.origin.yo
+    )
 
 init: (Model, Effects Action)
 init =
     EffectsUtil.update
-        (\x -> Model x NoOp (0, 0) {w = 0, h = 0})
+        (\x -> Model x NoOp (0, 0) {w = 0, h = 0} {xo = 0, yo = 0})
         GraphMapAction
         GraphMap.init
 
@@ -43,6 +53,7 @@ type Action
     | GraphMapAction GraphMap.Action
     | NodeMouseAction (Graph.NodeId, NodeBase.MouseAction)
     | DoubleClick
+    | Hold
     | Release
     | Resize (Int, Int)
 
@@ -54,21 +65,24 @@ update action model =
                 (\x -> { model | graphMap = x })
                 GraphMapAction
                 (GraphMap.update graphMapAction model.graphMap)
-        Move pos ->
+        Move (x, y) ->
             case model.state of
-                Connecting id pos' ->
-                    { model 
-                        | state = Connecting id pos
-                        , pos = pos }
-                    |> EffectsUtil.noFx
-                NoOp -> EffectsUtil.noFx { model | pos = pos }
+                MovingCamera (xm, ym) {xo, yo} ->
+                    EffectsUtil.noFx { model | origin = {xo = xo + x - xm, yo = yo + y - ym} }
+                _ -> EffectsUtil.noFx { model | mousePos = (x, y) }
         DoubleClick -> 
             GraphMap.update 
-                (Node.testNode model.pos |> GraphMap.AddNode)
+                (offsetMouse model |> Node.testNode |> GraphMap.AddNode)
                 model.graphMap
             |> EffectsUtil.update
                 (\x -> { model | graphMap = x })
                 GraphMapAction
+        Hold ->
+            case model.state of
+                NoOp ->
+                    EffectsUtil.noFx { model | state = MovingCamera model.mousePos model.origin }
+                _ ->
+                    EffectsUtil.noFx model
         Release ->
             EffectsUtil.noFx { model | state = NoOp }
         Resize (w, h) ->
@@ -82,17 +96,17 @@ update action model =
                                 Just pos -> pos
                                 Nothing -> Debug.log "mouse action no id" (0, 0)
                     in
-                        EffectsUtil.noFx { model | state = Connecting id (fst pos, snd pos) }
+                        EffectsUtil.noFx { model | state = Connecting id }
                 NodeBase.Up ->
                     case model.state of
-                        NoOp -> EffectsUtil.noFx model
-                        Connecting id' pos ->
+                        Connecting id' ->
                             GraphMap.update 
                                 (GraphMap.AddEdge id id' {})
                                 model.graphMap
                             |> EffectsUtil.update
                                 (\x -> { model | graphMap = x })
                                 GraphMapAction
+                        _ -> EffectsUtil.noFx model
 
 
 -- VIEW
@@ -102,17 +116,22 @@ view address model =
     let
         connection =
             case model.state of
-                Connecting id pos ->
-                    case Graph.get id model.graphMap.graph of
+                Connecting id ->
+                    case GraphMap.getNodePos id model.graphMap.graph of
                         Nothing -> []
-                        Just {incoming, node, outgoing} ->
-                            [ GraphMap.edgeForm node.label.pos pos ]
+                        Just nodePos ->
+                            [ GraphMap.edgeForm nodePos (offsetMouse model) ]
                 _ -> []
 
         svg =
             Svg.svg
                 [ toString model.size.w |> Att.width
                 , toString model.size.h |> Att.height
+                , CssUtil.style
+                    [ ipx model.origin.xo |> Css.left
+                    , ipx model.origin.yo |> Css.top
+                    , Css.position Css.absolute
+                    ]
                 ]
                 (
                     connection
@@ -127,9 +146,16 @@ view address model =
         Html.div 
             [ unselectableStyle
             , Events.onMouseUp address Release
+            , Events.onMouseDown address Hold
             , Events.onDoubleClick address DoubleClick
+            , CssUtil.style
+                [ Css.width (ipx model.size.w)
+                , Css.height (ipx model.size.h)
+                ]
             ]
-            [ svg ]
+            [ toString model.origin.xo ++ ", " ++ toString model.origin.yo |> Html.text 
+            , svg
+            ]
 
 unselectableStyle: Html.Attribute
 unselectableStyle =
