7869bdc0f58d803bd69e05b00abc99d017b2ab81
Sandi Dusic <dsandi333@gmail.com>
2017-11-21 23:49:04 +0100

    Add more functions for edge in graph manipulation

--- src/Util/Graph.elm
+++ src/Util/Graph.elm
@@ -52,3 +52,24 @@ getEdge : EdgeId -> Graph n e -> Maybe e
 getEdge { from, to } graph =
     Graph.get from graph
         |> Maybe.andThen (\ctx -> IntDict.get to ctx.outgoing)
+
+
+removeEdge : EdgeId -> Graph n e -> Graph n e
+removeEdge { from, to } graph =
+    let
+        updateCtx ctx =
+            { ctx | outgoing = IntDict.remove to ctx.outgoing }
+    in
+        Graph.update from (Maybe.map updateCtx) graph
+
+
+updateEdge : EdgeId -> (e -> e) -> Graph n e -> Graph n e
+updateEdge id update graph =
+    let
+        maybeEdge =
+            getEdge id graph |> Maybe.map update
+
+        updateCtx ctx =
+            { ctx | outgoing = IntDict.update id.to (always maybeEdge) ctx.outgoing }
+    in
+        Graph.update id.from (Maybe.map updateCtx) graph
