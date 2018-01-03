4cb2e7f4eecec4c684b6faa79c17d75316a8fb46
Sandi Dusic <dsandi333@gmail.com>
2018-01-04 00:37:35 +0100

    Add decode/encode for Vec2

--- src/Util.elm
+++ src/Util.elm
@@ -1,6 +1,10 @@
 module Util exposing (..)
 
 import Focus exposing (Focus)
+import Json.Decode as Decode
+import Json.Decode.Extra exposing ((|:))
+import Json.Encode as Encode
+import Math.Vector2 as Vec2 exposing (Vec2)
 
 
 nodeFocus : Focus { record | node : field } field
@@ -27,3 +31,18 @@ type alias Option msg =
 optionMap : (a -> b) -> Option a -> Option b
 optionMap f old =
     { old | msg = f old.msg }
+
+
+decodeVec2 : Decode.Decoder Vec2
+decodeVec2 =
+    Decode.succeed Vec2.vec2
+        |: Decode.field "x" Decode.float
+        |: Decode.field "y" Decode.float
+
+
+encodeVec2 : Vec2 -> Encode.Value
+encodeVec2 v =
+    Encode.object
+        [ ( "x", Vec2.getX v |> Encode.float )
+        , ( "y", Vec2.getY v |> Encode.float )
+        ]
