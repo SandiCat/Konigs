2e10047e99ae28a7e8fc8a7bbac804f74755eb34
Sandi Dusic <dsandi333@gmail.com>
2018-01-23 01:41:30 +0100

    Use webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+'use strict';
+import Elm from "./elm/Main.elm";
+import "./elm/Stylesheets.elm";
+
+var runningElmModule = Elm.Main.fullscreen();
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+  	contentBase: "./dist",
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/, /Stylesheets\.elm$/],
+        use: [
+          'elm-hot-loader',
+          'elm-webpack-loader',
+        ]
+      },
+      {
+        test: /Stylesheets\.elm$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'elm-css-webpack-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
+
+module.exports = config; 
