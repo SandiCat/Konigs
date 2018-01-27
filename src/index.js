fe316868d8da232806660cf1589074ff30bfed6c
Sandi Dusic <dsandi333@gmail.com>
2018-01-27 15:49:03 +0100

    Add localStorage support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 'use strict';
 import Elm from "./elm/Main.elm";
+import localStoragePorts from "elm-local-storage-ports";
 import "./elm/Stylesheets.elm";
 
 var runningElmModule = Elm.Main.fullscreen();
+localStoragePorts.register(runningElmModule.ports);
