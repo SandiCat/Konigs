b1ca0cf415e9daa18486931798e5022739fe32be
Sandi Dusic <dsandi333@gmail.com>
2018-04-09 17:21:21 +0200

    Revert "Add and set up webpack notify"
    This reverts commit 954c277a9da784f9042a171a3e5694bbd55ffb97.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const WebpackNotifierPlugin = require('webpack-notifier');
 
 const config = {
   entry: './src/index.js',
@@ -30,21 +29,12 @@ const config = {
           "postcss-loader",
           'elm-css-webpack-loader'
         ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          "postcss-loader"
-        ]
       }
     ]
   },
   plugins: [
     new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new WebpackNotifierPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ]
 };
 
